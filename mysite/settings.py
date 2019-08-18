11485948b322919fc0c7eeabad098c4ad5a71b2e
Navid Bahadoran <navid.bahadoran@gmail.com>
2019-08-17 17:49:34 -0700

    updated setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -19,11 +19,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wy!@#(&g&t))a!!iv9$vj+(8$e81u57d0@-+c#33y3pox7mm53'
-    # os.environ.get('SECRET_KEY')
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
+# 'wy!@#(&g&t))a!!iv9$vj+(8$e81u57d0@-+c#33y3pox7mm53'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG') == "TRUE")
+DEBUG = True
+    # (os.environ.get('DEBUG') == "True")
 
 ALLOWED_HOSTS = []
 # 'djangoblog-navid.herokuapp.com'
